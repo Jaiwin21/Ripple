57599d0d164e45180d282feb032fcdb9cbe2b168
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-05-27 16:57:02 +0100

    setup complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './Components/Homepage/HomePage';
+import Authetication from './Components/Authentication/Authentication';
 import './App.css';
 
 function App() {
   return (
-    <div className="">
+    <div className="">  
+
+    <Routes>
+      <Route path="/" element={true?<HomePage/>:<Authetication/>}>
+
+      </Route>
+    </Routes>
 
     </div>
   );
