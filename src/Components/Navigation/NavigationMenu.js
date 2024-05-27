eaa7f186dfe0514c58ffc75b8813490e8512edcb
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-05-27 17:22:33 +0100

    Icons

---
+++ src/Components/Navigation/NavigationMenu.js
@@ -0,0 +1,17 @@
+import HomeIcon from "@mui/icons-material/Home"
+import ExploreIcon from "@mui/icons-material/Explore"
+import NotificationIcon from "@mui/icons-material/Notification"
+import MessageIcon from '@mui/icons-material/Message';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import GroupIcon from '@mui/icons-material/Group';
+import VerifiedIcon from '@mui/icons-material/Verified';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import PendingIcon from '@mui/icons-material/Pending';
+
+export const navigation =[
+    {
+        title:"Home",
+        icon:<HomeIcon/>
+    }
+]
+
