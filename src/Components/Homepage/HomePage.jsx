e52aa3630a783a99de262aab6b3b8f227cc925e6
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-05-27 17:14:19 +0100

    Responsive functions added

--- src/Components/Homepage/HomePage.jsx
+++ src/Components/Homepage/HomePage.jsx
@@ -6,15 +6,15 @@ const HomePage = () => {
     
     <Grid container xs={12} classname='px-5 lg:px-36 justify-between'>
 
-        <Grid item xs={0} lg={2.5} classname='hidden lg:block w-full relative'>
+        <Grid item xs={0} lg={2.5} className='hidden lg:block w-full relative'>
         <p className='text-center'> left part</p>
         </Grid>
 
-        <Grid item xs={1} lg={5} classname='hidden lg:block w-full relative'>
+        <Grid item xs={1} lg={5} xl={6.5}className='hidden lg:block w-full relative'>
         <p className='text-center'> middle part</p>
         </Grid>
 
-        <Grid item xs={0} lg={3} classname='hidden lg:block w-full relative'>
+        <Grid item xs={0} lg={3} className='hidden lg:block w-full relative'>
         <p className='text-center'> right part</p>
         </Grid>
 
