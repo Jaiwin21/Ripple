ee11aaf8c27be3699ea74a51ae97d2d1c40154ab
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-05-27 16:43:52 +0100

    Adding backend

---
+++ ripple/src/test/java/ripple/example/ripple/RippleApplicationTests.java
@@ -0,0 +1,13 @@
+package ripple.example.ripple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RippleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
