5d5c2e14ed319075c1150c3f1c5d3e866cf993e2
Jaiwin21 <Jaiwinprince15@gmail.com>
2024-06-02 00:44:26 +0100

    Subscription plan

--- src/Components/SubscriptionModal/SubscriptionModal.jsx
+++ src/Components/SubscriptionModal/SubscriptionModal.jsx
@@ -24,6 +24,8 @@ export default function SubscriptionModal() {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
+    const [plan, setPlan]=React.useState("Anually");
+
     return (
         <div>
             <Button onClick={handleOpen}>Open modal</Button>
@@ -43,7 +45,20 @@ export default function SubscriptionModal() {
                     </div>
                     <div className='flex justify-center py-10'>
                         <div className='w-[80%] space-y-10'>
-                            <div className='p-5 rounded-md flex items-center justify-between'>
+                            <div className='p-5 rounded-md flex items-center justify-between bg-slate-400 shadow-lg'>
+                                <h1 className='text-xl pr-5'>Subscribers will get a blue checkmark to show off.</h1>
+                                <img className='w-24 h-24' src="https://abs.twimg.com/responsive-web/client-web/verification-card-v2@3x.8ebee01a.png" alt="" />
+
+                            </div>
+
+                            <div className='flex justify-between border rounded-full px-5 py-3'>
+                                <div>
+                                    <span>Anually</span>
+                                    <span>SAVE 18%</span>
+                                </div>
+                                <p className={`${plan==="monthly"}`}>
+                                    Monthly
+                                </p>
 
                             </div>
 
